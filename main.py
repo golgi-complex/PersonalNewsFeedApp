261a36f7fbb8d4ecaa0ecfbe9fd4031ff19b972b
ilya.kovalev88@gmail.com <0okmjy6tfcde3>
2026-08-25 18:37:16 +0300

    fix: Replace invalid os.parse_env with os.getenv

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telethon.errors import FloodWaitError, ChannelPrivateError, UsernameInvalid
 
 load_dotenv()
 
-API_ID = int(os.parse_env('API_ID') if os.getenv('API_ID') else os.getenv('API_ID'))
+API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_ID = int(os.getenv('ADMIN_ID'))
